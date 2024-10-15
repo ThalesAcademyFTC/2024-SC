1b2ae3e8263cb4d6cbd82b4617aa5e628546de0a
ethanvader <93680550+ethanvader@users.noreply.github.com>
2024-10-15 16:39:28 -0400

    Co-authored-by: Yoshi <thisrandomexistantguy@users.noreply.github.com> Co-authored-by: TheRealSkibidi <TheRealSkibidi@users.noreply.github.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Johnny7AutonTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Johnny7AutonTest.java
@@ -3,15 +3,21 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+
 Autonomous()
 public class Johnny7AutonTest {
-    private Johnny6 johnny6;
+    private Johnny6 johnny7;
 
     private ElapsedTime runtime=new ElapsedTime();
 
     @Override
     public void runOpMode() {
-        johnny6 = new Johnny6(this, Johnny6.DriveTrain.JOHNNY6);
+        johnny7 = new Johnny6(this, Johnny6.DriveTrain.JOHNNY6);
+
+        waitForStart();
+
+        runtime.reset();
+        
+        johnny7.moveForwardInches(10,.5);
     }
 }
-come
